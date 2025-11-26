1d48ebf18f315d17178a0d8bc2852112bb70e718
RHallberg <hallbergrickard@hotmail.com>
2025-11-26 18:21:13 +0100

    changes to appearance

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka NFM Heavy:size=15"
+	"Iosevka Nerd Font Mono:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#4c4f69", "#e6e9ef" },
+	[SchemeSel] = { "#1E66F5", "#bcc0cc" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
